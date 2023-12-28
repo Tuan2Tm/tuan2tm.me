3e5e1b434f1b101bc09c04fc62fe91b45ed48391
Tuan2tm <tuantrinh28072002@gmail.com>
2023-12-28 13:57:38 +0700

    demo using local

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,30 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
-
-import "./globals.css";
 import { Header, ThemeProvider } from "@/components";
 import useLang from "@/hooks/useLang";
 import Head from "next/head";
-import { useEffect } from "react";
 import { I18nextProvider } from "react-i18next";
+import "./globals.css";
+import i18next from "i18next";
+import { useEffect } from "react";
+import { load } from "@/utils/storage";
+import { DEFAULT_LANG, KEY_LANG } from "@/constants/i18n";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { i18next } = useLang();
-  useEffect(() => {
-    document.documentElement.lang = i18next.language;
-    console.log('call back');
-    
-  }, [i18next.language]);
+  // const langLocal = load(KEY_LANG);
+  // const lang = langLocal || DEFAULT_LANG;
+  // const { initI18n } = useLang(lang);
+
+  // useEffect(() => {
+  //   initI18n(lang);
+  // }, []);
 
   return (
-    <html lang={i18next.language} suppressHydrationWarning>
+    <html lang={DEFAULT_LANG} suppressHydrationWarning>
       <Head>
         <title>My page title</title>
       </Head>
--- src/hooks/useLang.ts
+++ src/hooks/useLang.ts
@@ -1,48 +1,43 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import i18next from "i18next";
-import { DEFAULT_LANG } from "@/constants/i18n";
-import I18NextHttpBackend from "i18next-http-backend";
-import { initReactI18next, useTranslation } from "react-i18next";
+import { DEFAULT_LANG, KEY_LANG } from "@/constants/i18n";
+import { initReactI18next } from "react-i18next";
 import { enLang, viLang } from "@/i18n";
+import { save } from "@/utils/storage";
 
 const resources = Object.freeze({
-  vi: {translation: viLang},
-  en: {translation: enLang},
+  vi: { translation: viLang },
+  en: { translation: enLang },
 });
 
-
-const useLang = () => {
-
-  i18next
-    .use(I18NextHttpBackend)
-    .use(initReactI18next)
-    .init({
-      resources,
-      lng: DEFAULT_LANG,
-      fallbackLng: "en",
-      debug: false,
-      react: {
-        useSuspense: false,
-      },
-      interpolation: {
-        escapeValue: false,
-        formatSeparator: ",",
-      },
-    })
-    .then(() => {
-      console.log("initialI18Next success!");
-    })
-    .catch((e) => {
-      console.log("initialI18Next error", e);
-    });
-
-  const onChangeLang = (code: string): void => {
-    console.log(code);
-    
-    i18next.changeLanguage(code);
+const useLang = (langLocal?: string) => {
+  const initI18n = (lang?: string) => {
+    i18next
+      .use(initReactI18next)
+      .init({
+        resources,
+        lng: lang || DEFAULT_LANG,
+        fallbackLng: DEFAULT_LANG,
+        debug: false,
+        react: {
+          useSuspense: true,
+        },
+      })
+      .then(() => {
+        console.log("initialI18Next success!");
+      })
+      .catch((e) => {
+        console.log("initialI18Next error", e);
+      });
   };
 
-  return {  i18next, onChangeLang };
+  langLocal ?? save(KEY_LANG, DEFAULT_LANG);
+
+  const onChangeLang = (code: string): void => {
+    i18next.changeLanguage(code);
+    save(KEY_LANG, code);
+  };
+
+  return { initI18n, onChangeLang };
 };
 
 export default useLang;
